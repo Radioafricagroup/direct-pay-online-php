82d17f440395491fab4e46cb0258ddaeb10c9fe4
tiras.muturi <tiras.muturi@radioafricagroup.co.ke>
2023-06-08 11:15:46 +0300

    added nwe values Ok dpo

--- src/Rag/DirectPayOnline/Request/OkConfirmationRequest.php
+++ src/Rag/DirectPayOnline/Request/OkConfirmationRequest.php
@@ -12,6 +12,41 @@ use Rag\DirectPayOnline\Request;
 
 class OkConfirmationRequest extends Request{
 
+    private $_transactionRef = "";
+    private $_companyRef = "";
+
+    /**
+     * @return string
+     */
+    function getTransactionRef()
+    {
+        return $this->_transactionRef;
+    }
+
+    /**
+     * @param string $transactionRef
+     */
+    function setTransactionRef($transactionRef)
+    {
+        $this->_transactionRef = $transactionRef;
+    }
+
+    /**
+     * @return string
+     */
+    function getCompanyRef()
+    {
+        return $this->_companyRef;
+    }
+
+    /**
+     * @param string $companyRef
+     */
+    function setCompanyRef($companyRef)
+    {
+        $this->_companyRef = $companyRef;
+    }
+
     function execute()
     {
         $xml = "";
@@ -19,6 +54,8 @@ class OkConfirmationRequest extends Request{
         $xml .= '<?xml version="1.0" encoding="utf-8"?>';
         $xml .= "<API3G>";
         $xml .= "<Response>OK</Response>";
+        $xml .= "<CompanyRef>{$this->getCompanyRef()}</CompanyRef>";
+        $xml .= "<AccRef>{$this->getTransactionRef()}</AccRef>";
         $xml .= "</API3G>";
 
         return $this->_client->post("/v6/", $xml); // Do not delete the slash of the end
