7d571d97ccac6cccc84520a010e883f96e76db69
tiras.muturi <tiras.muturi@radioafricagroup.co.ke>
2023-09-19 15:15:26 +0300

    added refund Dpo + changes

--- src/Rag/DirectPayOnline/Request/RefundTokenRequest.php
+++ src/Rag/DirectPayOnline/Request/RefundTokenRequest.php
@@ -95,16 +95,16 @@ class RefundTokenRequest extends Request
 
 		$xml .= '<?xml version="1.0" encoding="utf-8"?>';
 		$xml .= "<API3G>";
+        $xml .= "<Request>refundToken</Request>";
 		$xml .= "<CompanyToken>{$this->_config->getCompanyToken()}</CompanyToken>";
-		$xml .= "<Request>refundToken</Request>";
+        if($this->getTransactionToken()) {
+            $xml .= "<TransactionToken>{$this->getTransactionToken()}</TransactionToken>";
+        }
+        if($this->getCompanyRef()) {
+            $xml .= "<CompanyRef>{$this->getTransactionToken()}</CompanyRef>";
+        }
 		$xml .= "<refundAmount>{$this->getRefundAmount()}</refundAmount>";
-		$xml .= "<refundDetails>Refund description</refundDetails>";
-		if($this->getTransactionToken()) {
-			$xml .= "<TransactionToken>{$this->getTransactionToken()}</TransactionToken>";
-		}
-		if($this->getCompanyRef()) {
-			$xml .= "<CompanyRef>{$this->getTransactionToken()}</CompanyRef>";
-		}
+		$xml .= "<refundDetails>{$this->getRefundDescription()}</refundDetails>";
 		$xml .= "</API3G>";
 
 		return $this->_client->post("/v7/", $xml); // Do not delete the slash of the end
